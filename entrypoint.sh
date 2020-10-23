4c0adb822d7d306fef66b1210068264e7da4683d
Remi de Boer <r.d.d.de.boer@hva.nl>
2020-10-23 12:18:25 +0200

    WIP: added copying from source repo to destination repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,21 @@
 set -e
 set -x
 
+if [ -z "$INPUT_SOURCE_REPO" ]
+then
+  echo "Source repo must be defined"
+  return -1
+fi
+
 if [ -z "$INPUT_SOURCE_FOLDER" ]
 then
   echo "Source folder must be defined"
+  return -1
+fi
+
+if [ -z "$INPUT_DESTINATION_REPO" ]
+then
+  echo "Destination repo folder must be defined"
   return -1
 fi
 
@@ -14,25 +26,34 @@ then
   INPUT_DESTINATION_BRANCH=main
 fi
 
+if [ -z "$INPUT_DESTINATION_BRANCH" ]
+then
+  INPUT_SOURCE_BRANCH=main
+fi
+
 if [ -z "$INPUT_COMMIT_MSG" ]
 then
   INPUT_COMMIT_MSG="Update $INPUT_DESTINATION_FOLDER."
 fi
 
-CLONE_DIR=$(mktemp -d)
+CLONE_DIR_SRC=$(mktemp -d)
+CLONE_DIR_DST=$(mktemp -d)
 
 echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+git clone --single-branch --branch $INPUT_SOURCE_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_SOURCE_REPO.git" "$CLONE_DIR_SRC"
+git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR_DST"
 
-echo "Copying contents to git repo"
-rm -rf $CLONE_DIR
-cp -a $INPUT_SOURCE_FOLDER/. $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
-cd "$CLONE_DIR"
+echo "Removing old folder: ${$CLONE_DIR_DST/$INPUT_DESTINATION_FOLDER/}"
+rm -rf $CLONE_DIR_DST/$INPUT_DESTINATION_FOLDER/
+echo "Copying: ${$CLONE_DIR_SRC/$INPUT_SOURCE_FOLDER/} -> ${$CLONE_DIR_DST/$INPUT_DESTINATION_FOLDER/}"
+cp -a $CLONE_DIR_SRC/$INPUT_SOURCE_FOLDER/. $CLONE_DIR_DST/$INPUT_DESTINATION_FOLDER/
+echo "switchin to destination repo directory: ${$CLONE_DIR_DST}"
+cd "$CLONE_DIR_DST"
+ls -a
 
 echo "Adding git commit"
-git add .
 if git status | grep -q "Changes to be committed"
 then
   git commit --message "$INPUT_COMMIT_MSG"
